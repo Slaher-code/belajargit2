28fb97cc1fc9eceff34545ea0ed7ff4ac455a95d
ibnusample <ibnu@example.com>
2025-03-19 15:57:04 +0700

    Revisi TambahAngka

--- coba java/aksi.js
+++ coba java/aksi.js
@@ -1,5 +1,10 @@
+const tambahAngka = (a,b) => a + b;
+console.log(tambahAngka(2,5));
+
 const pengurangan = (a,b) => a - b;
 console.log(pengurangan(4,2));
 
 const perkalian = (a,b) => a * b;
 console.log(perkalian(2,2));
+
+console.log("Ini masas lalu")
